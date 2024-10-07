1f92724ef92a7d61c061fc1e3afabc631fd00aac
koraybiberoglu <korayttoprak@gmail.com>
2024-10-07 20:40:30 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
         public static void main(String[] args) {
             Adder adder = new Adder();
-            System.out.println(adder.add(1, 2));
+            System.out.println(adder.add(15, 20));
             Subtractor subtractor = new Subtractor();
-            System.out.println(subtractor.subtract(6, 3));
+            System.out.println(subtractor.subtract(70, 105));
             System.out.println("IZMIR GENCLIK KOLLARI");
         }
     }
